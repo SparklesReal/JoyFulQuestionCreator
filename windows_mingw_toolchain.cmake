c0bba436dfe34d458add94baff6c6b8155b3bfc2
Sparkles <jultomtenriktig@gmail.com>
2024-07-14 18:35:43 +0200

    Cmake

--- windows_mingw_toolchain.cmake
+++ windows_mingw_toolchain.cmake
@@ -1,24 +1,37 @@
-# Sample toolchain file for building for Windows from an Ubuntu Linux system.
-#
-# Typical usage:
-#    *) install cross compiler: `sudo apt-get install mingw-w64`
-#    *) cd build
-#    *) cmake -DCMAKE_TOOLCHAIN_FILE=../mingw_toolchain.cmake ..
-# This is free and unencumbered software released into the public domain.
+# Sample toolchain file for cross-compiling to Windows from Ubuntu Linux with mingw-w64.
 
+# Set the system name to Windows
 set(CMAKE_SYSTEM_NAME Windows)
+
+# Specify the toolchain prefix
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
-# cross compilers to use for C, C++ and Fortran
+# Set cross compilers for C and C++
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}-gfortran)
-set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
-# target environment on the build host system
+# Set the find root path
 set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
 
-# modify default behavior of FIND_XXX() commands
+# Modify default behavior of FIND_XXX() commands
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Additional settings you might need
+# Example:
+# set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+
+# If necessary, add include directories for mingw-w64 headers
+# include_directories(/usr/${TOOLCHAIN_PREFIX}/include)
+
+# If necessary, add link directories for mingw-w64 libraries
+# link_directories(/usr/${TOOLCHAIN_PREFIX}/lib)
+
+# Example: Set compilation flags
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
+
+# Example: Set linker flags
+# set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++")
+
+# End of toolchain file
